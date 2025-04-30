9b7f4da672e659e56918da795c61225519bce7a4
rabowling <reedbowling@gmail.com>
2025-04-30 00:49:39 -0500

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is eventually going to be my website!
+
+          I am going to learn React!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
